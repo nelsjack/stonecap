6b4558c567249a03b23d7b7fbf7187dc02928dab
Jack Nelson <jackrnelson47@gmail.com>
2023-01-31 15:11:16 -0500

    insert boardgame data into boardgame table

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -3,4 +3,10 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO boardgames (user_id, boardgame_id, save_type) VALUES ('1', 'TAAifFP590', 'played');
+INSERT INTO boardgames (user_id, boardgame_id, save_type) VALUES ('1', 'yqR4PtpO8X', 'played');
+INSERT INTO boardgames (user_id, boardgame_id, save_type) VALUES ('1', '5H5JS0KLzK', 'played');
+INSERT INTO boardgames (user_id, boardgame_id, save_type) VALUES ('2', 'RLlDWHh7hR', 'played');
+INSERT INTO boardgames (user_id, boardgame_id, save_type) VALUES ('2', 'fDn9rQjH9O', 'played');
+
 COMMIT TRANSACTION;
