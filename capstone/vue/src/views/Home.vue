06a24bc629d868fe1a63c9c22f979d3b8ac13d4f
Jack Nelson <jackrnelson47@gmail.com>
2023-01-31 11:15:28 -0500

    add router links to login and register from home

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <p>Token: {{ this.$store.state.token }} </p>
+    <router-link v-bind:to="{ name: 'login'}"> 
+      <button>Login</button>
+    </router-link>
+    <router-link v-bind:to="{ name: 'register'}">
+      <button>Register</button>
+    </router-link>
   </div>
 </template>
 
