58b7f613948cbacd3f1cd6cb72a0cef85c2b6a37
Jack Nelson <jackrnelson47@gmail.com>
2023-02-01 10:33:07 -0500

    fix axios calls and display user's game collection

---
+++ capstone/vue/src/components/GameCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="game-card" v-for="game in games" v-bind:key="game.id">
+      <img class="game-thumbnail" :src="game.thumb_url" />
+      <h3 class="game-title">{{ game.handle }}</h3>
+      <p class="game-description">{{ game.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'game-card',
+    props: ['games']
+};
+</script>
+
+<style>
+  .game-thumbnail {
+    height: 5em;
+    width: 5em;
+    margin-left: 5px;
+  }
+
+  .trending-games-container {
+    width: 75%;
+    margin-top: 1em;
+  }
+
+  .game-card {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    border: solid 1px;
+    border-radius: 10px;
+    margin-top: 10px;
+  }
+
+  .game-title {
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  .game-description {
+    margin-right: 5px;
+  }
+</style>
