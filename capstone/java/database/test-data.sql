6649afc5a623cf5dbf3f51091fa11332d3d13d1b
Nicholas Scacchetti <nscacchetti@gmail.com>
2023-02-02 10:33:54 -0500

    Adding user test data

--- capstone/java/database/test-data.sql
+++ capstone/java/database/test-data.sql
@@ -5,7 +5,7 @@ INSERT INTO users (username,password_hash,role) VALUES ('user2','$2a$10$9pLqNb.V
 INSERT INTO users (username,password_hash,role) VALUES ('user3','$2a$10$HuH19ioVrKFtJenqVDZTwuNS2Nr2Vc33WZALPE.PoBIbDDGeaYrtO','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('user4','$2a$10$9pLqNb.VJjyFF0Slh7lF/Oh36b1AO730phG6qRwqHcL5kpz2RmF3a','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('user5','$2a$10$HuH19ioVrKFtJenqVDZTwuNS2Nr2Vc33WZALPE.PoBIbDDGeaYrtO','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('test','$2a$10$9pLqNb.VJjyFF0Slh7lF/Oh36b1AO730phG6qRwqHcL5kpz2RmF3a','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$10$HuH19ioVrKFtJenqVDZTwuNS2Nr2Vc33WZALPE.PoBIbDDGeaYrtO','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('user6','$2a$10$9pLqNb.VJjyFF0Slh7lF/Oh36b1AO730phG6qRwqHcL5kpz2RmF3a','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$10$HuH19ioVrKFtJenqVDZTwuNS2Nr2Vc33WZALPE.PoBIbDDGeaYrtO','ROLE_ADMIN');
 
 COMMIT TRANSACTION;
