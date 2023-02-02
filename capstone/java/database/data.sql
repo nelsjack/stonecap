6f16696a194d79c8c4387a918dbe10572a116c95
Nicholas Scacchetti <nscacchetti@gmail.com>
2023-02-02 12:04:55 -0500

    create post update data.sql

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -1,12 +1,25 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+--Add users
+INSERT INTO users (username,password_hash,role) VALUES ('user1','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('user2','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+--Fill Boardgames with played, wishlist and owned
 INSERT INTO boardgames (user_id, board_game_id, save_type) VALUES ('1', 'TAAifFP590', 'played');
 INSERT INTO boardgames (user_id, board_game_id, save_type) VALUES ('1', 'yqR4PtpO8X', 'played');
-INSERT INTO boardgames (user_id, board_game_id, save_type) VALUES ('1', '5H5JS0KLzK', 'played');
+INSERT INTO boardgames (user_id, board_game_id, save_type) VALUES ('1', '3IPVIROfvl', 'wishlist');
+INSERT INTO boardgames (user_id, board_game_id, save_type) VALUES ('1', 'OIXt3DmJU0', 'wishlist');
+INSERT INTO boardgames (user_id, board_game_id, save_type) VALUES ('1', '5H5JS0KLzK', 'owned');
+INSERT INTO boardgames (user_id, board_game_id, save_type) VALUES ('1', 'M7r9tO1GbX', 'owned');
 INSERT INTO boardgames (user_id, board_game_id, save_type) VALUES ('2', 'RLlDWHh7hR', 'played');
 INSERT INTO boardgames (user_id, board_game_id, save_type) VALUES ('2', 'fDn9rQjH9O', 'played');
+
+--Create friends entry
+INSERT INTO friends (user_id_one, user_id_two) VALUES ('1','2');
+
+--Create a post
+INSERT INTO post (user_id, user_game_id, image, title, comments, tags, rating, public_private) VALUES ('1','2','https://cf.geekdo-images.com/hyqVOyVvyUAVu3PmlP9scg__imagepage/img/-UKJcRC6XhCjZ7JzC6u7m9JRq88=/fit-in/900x600/filters:no_upscale():strip_icc()/pic2977400.jpg');
+
 
 COMMIT TRANSACTION;
