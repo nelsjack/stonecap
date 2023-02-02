58319ea96c9937f7dcc864be74abddc0fe07310c
Jack Nelson <jackrnelson47@gmail.com>
2023-02-02 09:40:31 -0500

    setup up BootstrapVue and start navbar

---
+++ capstone/vue/src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-navbar>
+      <b-navbar-nav>
+          <b-nav-item to="{ name: 'login'}"> 
+      </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
